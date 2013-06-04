b1276fe5a33d8690a1f5d392c1524c84dfa3e242
Jarrod Taylor <Jarrod.C.Taylor@gmail.com>
2013-06-04 18:31:25 -0500

    Modify config_linux_enviornment.sh
    Modify config_linux_enviornment.sh to install nodejs from github
    so we have a current version which will properly run coffee script.
    
    Modify config_linux_enviornment to write progress to a log file.
    This way if there is a problem with the install we can see where it
    failed. The script prints the log file at the end to give the user
    a summary of what has been installed. The log file is then deleted.

--- config_linux_enviornment.sh
+++ config_linux_enviornment.sh
@@ -21,22 +21,22 @@
 #==============
 # Variables
 #==============
-dir=~/dotfiles           # Dotfiles directory
-files="bashrc vimrc vim emacs.d" # List of files / folders to symlink in home folder
+dir=~/dotfiles                         # Dotfiles directory
+files="bashrc vimrc vim emacs.d"       # List of files / folders to symlink in home folder
+log_file="~/install_progress_log.txt"  # Does what it says on the tin
 
 #==============
 # Change to the dotfiles directory
 #==============
-echo -n "Changing to the $dir directory ..."
+echo -n "Changing to the $dir directory ..." >> log_file
 cd $dir
-echo "done"
 
 #==============
 # Create symlinks in the home folder to any files in the ~/dotfiles folder 
 # that we specified in $files variable.
 #==============
 for file in $files; do
-    echo "Creating symlink to $file in $HOME"
+    echo "Creating symlink to $file in $HOME" >> log_file
     ln -s $dir/$file ~/.$file
 done
 
@@ -44,58 +44,97 @@ done
 # Clone vundle so we can update vim plugins when we open it 
 # the first time
 #==============
-echo "Cloning vundle to manage vim plugins"
+echo "Cloning vundle to manage vim plugins" >> log_file
 git clone https://github.com/gmarik/vundle.git ~/dotfiles/vim/bundle/vundle
+echo "Vundle successfully cloned" >> log_file
 
 #==============
 # Ask if user wants to install all the required and additional packages
 #==============
 echo "Would you like to install all additional packages for this setup (Y/n) => "; read answer
 if [[ $answer = "Y" ]] ; then
-    echo "Installing Vim"
+    echo "Installing Vim" >> log_file
     sudo apt-get install vim-gnome
-    echo "Installing Emacs"
+    echo "Vim Installed" >> log_file
+
+    echo "Installing Emacs" >> log_file
     sudo apt-get install emacs24
-    echo "Installing Keepassx"
+    echo "Emacs Installed" >> log_file
+
+    echo "Installing Keepassx" >> log_file
     sudo apt-get install keepassx
-    echo "Installing figlet"
+    echo "Keepassx Installed" >> log_file
+
+    echo "Installing figlet" >> log_file
     sudo apt-get install figlet
-    echo "Installing espeak"
+    echo "figlet Installed" >> log_file
+
+    echo "Installing espeak" >> log_file
     sudo apt-get install espeak
-    echo "Installing curl"
+    echo "espeak Installed" >> log_file
+
+    echo "Installing curl" >> log_file
     sudo apt-get install curl
-    echo "Installing exuberant-ctags"
+    echo "curl Installed" >> log_file
+
+    echo "Installing exuberant-ctags" >> log_file
     sudo apt-get install exuberant-ctags
-    echo "Downloading and installing git-completion"
+    echo "exuberant-ctags Installed" >> log_file
+
+    echo "Downloading and installing git-completion" >> log_file
     cd ~/
     curl -OL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
     mv ~/git-completion.bash ~/.git-completion.bash
-    echo "Installing nodejs, npm and jshint for js error checking in vim, and coffee script"
-    # Currently I am not doing node development. It should
-    # be noted that the Debian version fo nodejs is very out of date
-    # so if node development is of interest it would be better to build 
-    # from source
-    sudo apt-get install nodejs
+    echo "git-completion Installed and Configured" >> log_file
+
+    echo "Installing nodejs" >> log_file
+    cd ~/
+    git clone https://github.com/joyent/node
+    cd node
+    ./configure --prefix=/usr/local
+    make
+    sudo make install
+    echo "nodejs Installed" >> log_file
+
+    echo "Installing npm" >> log_file
     sudo apt-get install npm
+    echo "npm Installed" >> log_file
+    
+    echo "Installing jshint" >> log_file
     sudo npm install -g jshint
+    echo "jshint Installed" >> log_file
+
+    echo "Installing coffee script" >> log_file
     sudo npm install -g coffee-script
+    echo "coffee script Installed" >> log_file
+
     # jshint will look for node and it seems to be installed as
     # nodejs if that is the case we create a symlink
-    if [ ! -f /usr/bin/node ]; then 
-        sudo ln -s /usr/bin/nodejs /usr/bin/node
-    fi
-    echo "Installing pyflakes for python error checking in vim"
+    ##if [ ! -f /usr/bin/node ]; then 
+    ##    sudo ln -s /usr/bin/nodejs /usr/bin/node
+    ##fi
+
+    echo "Installing pyflakes" >> log_file
     sudo apt-get install pyflakes
-    echo "Downloading and installing Ack"
+    echo "pyflakes Installed" >> log_file
+
+    echo "Downloading and installing Ack" >> log_file
     curl http://beyondgrep.com/ack-2.04-single-file > ~/ack && chmod 0755 !#:3 
     sudo mv ~/ack /usr/bin/ack
-    echo "Installing pip"
+    echo "Ack Downloaded and Installed" >> log_file
+
+    echo "Installing pip" >> log_file
     sudo apt-get install python-pip
+    echo "pip Installed" >> log_file
 fi
 
 #==============
-# Wrap it up with warm fuzzies
+# Give the user a summary of what has been installed
 #==============
-echo "====== Finished ======"
+echo -e "\n====== Success!! ======\n"
+echo -e "\n====== Summary ======\n"
+cat log_file
+echo
 echo "When you first open vim run :BundleInstall to install the plugins"
-echo "All done enjoy"
+echo "Enjoy -Jarrod"
+rm log_file
