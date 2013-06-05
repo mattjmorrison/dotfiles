29f7d6eb795774ba4d3c8d7d02ce99d1c557176c
Jarrod Taylor <Jarrod.C.Taylor@gmail.com>
2013-06-04 21:34:12 -0500

    Modify config_linux_enviornment.sh
    log_file variable was not pointing to the same file every time it
    was being written to. Variable reference corrected

--- config_linux_enviornment.sh
+++ config_linux_enviornment.sh
@@ -23,12 +23,12 @@
 #==============
 dir=~/dotfiles                         # Dotfiles directory
 files="bashrc vimrc vim emacs.d"       # List of files / folders to symlink in home folder
-log_file="~/install_progress_log.txt"  # Does what it says on the tin
+log_file=~/install_progress_log.txt    # Does what it says on the tin
 
 #==============
 # Change to the dotfiles directory
 #==============
-echo -n "Changing to the $dir directory ..." >> log_file
+echo -n "Changing to the $dir directory ..." >> $log_file
 cd $dir
 
 #==============
@@ -36,7 +36,7 @@ cd $dir
 # that we specified in $files variable.
 #==============
 for file in $files; do
-    echo "Creating symlink to $file in $HOME" >> log_file
+    echo "Creating symlink to $file in $HOME" >> $log_file
     ln -s $dir/$file ~/.$file
 done
 
@@ -44,69 +44,69 @@ done
 # Clone vundle so we can update vim plugins when we open it 
 # the first time
 #==============
-echo "Cloning vundle to manage vim plugins" >> log_file
+echo "Cloning vundle to manage vim plugins" >> $log_file
 git clone https://github.com/gmarik/vundle.git ~/dotfiles/vim/bundle/vundle
-echo "Vundle successfully cloned" >> log_file
+echo "Vundle successfully cloned" >> $log_file
 
 #==============
 # Ask if user wants to install all the required and additional packages
 #==============
 echo "Would you like to install all additional packages for this setup (Y/n) => "; read answer
 if [[ $answer = "Y" ]] ; then
-    echo "Installing Vim" >> log_file
+    echo "Installing Vim" >> $log_file
     sudo apt-get install vim-gnome
-    echo "Vim Installed" >> log_file
+    echo "Vim Installed" >> $log_file
 
-    echo "Installing Emacs" >> log_file
+    echo "Installing Emacs" >> $log_file
     sudo apt-get install emacs24
-    echo "Emacs Installed" >> log_file
+    echo "Emacs Installed" >> $log_file
 
-    echo "Installing Keepassx" >> log_file
+    echo "Installing Keepassx" >> $log_file
     sudo apt-get install keepassx
-    echo "Keepassx Installed" >> log_file
+    echo "Keepassx Installed" >> $log_file
 
-    echo "Installing figlet" >> log_file
+    echo "Installing figlet" >> $log_file
     sudo apt-get install figlet
-    echo "figlet Installed" >> log_file
+    echo "figlet Installed" >> $log_file
 
-    echo "Installing espeak" >> log_file
+    echo "Installing espeak" >> $log_file
     sudo apt-get install espeak
-    echo "espeak Installed" >> log_file
+    echo "espeak Installed" >> $log_file
 
-    echo "Installing curl" >> log_file
+    echo "Installing curl" >> $log_file
     sudo apt-get install curl
-    echo "curl Installed" >> log_file
+    echo "curl Installed" >> $log_file
 
-    echo "Installing exuberant-ctags" >> log_file
+    echo "Installing exuberant-ctags" >> $log_file
     sudo apt-get install exuberant-ctags
-    echo "exuberant-ctags Installed" >> log_file
+    echo "exuberant-ctags Installed" >> $log_file
 
-    echo "Downloading and installing git-completion" >> log_file
+    echo "Downloading and installing git-completion" >> $log_file
     cd ~/
     curl -OL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
     mv ~/git-completion.bash ~/.git-completion.bash
-    echo "git-completion Installed and Configured" >> log_file
+    echo "git-completion Installed and Configured" >> $log_file
 
-    echo "Installing nodejs" >> log_file
+    echo "Installing nodejs" >> $log_file
     cd ~/
     git clone https://github.com/joyent/node
     cd node
     ./configure --prefix=/usr/local
     make
     sudo make install
-    echo "nodejs Installed" >> log_file
+    echo "nodejs Installed" >> $log_file
 
-    echo "Installing npm" >> log_file
+    echo "Installing npm" >> $log_file
     sudo apt-get install npm
-    echo "npm Installed" >> log_file
+    echo "npm Installed" >> $log_file
     
-    echo "Installing jshint" >> log_file
+    echo "Installing jshint" >> $log_file
     sudo npm install -g jshint
-    echo "jshint Installed" >> log_file
+    echo "jshint Installed" >> $log_file
 
-    echo "Installing coffee script" >> log_file
+    echo "Installing coffee script" >> $log_file
     sudo npm install -g coffee-script
-    echo "coffee script Installed" >> log_file
+    echo "coffee script Installed" >> $log_file
 
     # jshint will look for node and it seems to be installed as
     # nodejs if that is the case we create a symlink
@@ -114,18 +114,18 @@ if [[ $answer = "Y" ]] ; then
     ##    sudo ln -s /usr/bin/nodejs /usr/bin/node
     ##fi
 
-    echo "Installing pyflakes" >> log_file
+    echo "Installing pyflakes" >> $log_file
     sudo apt-get install pyflakes
-    echo "pyflakes Installed" >> log_file
+    echo "pyflakes Installed" >> $log_file
 
-    echo "Downloading and installing Ack" >> log_file
+    echo "Downloading and installing Ack" >> $log_file
     curl http://beyondgrep.com/ack-2.04-single-file > ~/ack && chmod 0755 !#:3 
     sudo mv ~/ack /usr/bin/ack
-    echo "Ack Downloaded and Installed" >> log_file
+    echo "Ack Downloaded and Installed" >> $log_file
 
-    echo "Installing pip" >> log_file
+    echo "Installing pip" >> $log_file
     sudo apt-get install python-pip
-    echo "pip Installed" >> log_file
+    echo "pip Installed" >> $log_file
 fi
 
 #==============
@@ -133,8 +133,8 @@ fi
 #==============
 echo -e "\n====== Success!! ======\n"
 echo -e "\n====== Summary ======\n"
-cat log_file
+cat $log_file
 echo
 echo "When you first open vim run :BundleInstall to install the plugins"
 echo "Enjoy -Jarrod"
-rm log_file
+rm $log_file
