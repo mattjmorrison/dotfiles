818fb7b2ae19f46192e78be70079ef48a9f5fdd0
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2014-10-05 14:52:01 -0500

    Enhancement: Added lein kibit and midje to profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,3 @@
-{:user {:plugins [[lein-kibit "0.0.8"]]
+{:user {:plugins [[lein-kibit "0.0.8"]
+                  [lein-midje "3.1.3"]]
         :repl-options {:prompt (fn [ns] (str "λ " ns " ─➤ "))}}}
