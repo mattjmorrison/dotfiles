99444abdbda4d18b9f270bfefbf6e38c88965551
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2014-11-22 09:06:21 -0600

    Enhancement: Added Slamhound to profiles.clj and vim

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,4 @@
-{:user {:plugins [[lein-kibit "0.0.8"]]
+{:user {:dependencies [[slamhound "1.5.5"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :plugins [[lein-kibit "0.0.8"]]
         :repl-options {:prompt (fn [ns] (str "λ " ns " ─➤ "))}}}
