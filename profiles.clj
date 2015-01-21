d28a4290c87316661b873e62d9c7932bffe0a327
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-01-21 17:31:42 -0600

    Enhancement: Added humane test output clojure plugin

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,8 @@
-{:user {:dependencies [[slamhound "1.5.5"]]
+{:user {:dependencies [[slamhound "1.5.5"]
+                       [pjstadig/humane-test-output "0.6.0"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :plugins [[lein-kibit "0.0.8"]]
-        :repl-options {:prompt (fn [ns] (str "λ " ns " ─➤ "))}}}
+        :plugins [[lein-kibit "0.0.8"]
+                  [com.jakemccrary/lein-test-refresh "0.5.5"]]
+        :repl-options {:prompt (fn [ns] (str "λ " ns " ─➤ "))}
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
