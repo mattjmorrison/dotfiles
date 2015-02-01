7d4bb89d46edbe6419e9cf0d6da02ee5cb822334
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-02-01 15:37:15 -0600

    Enhancement: Giving ultra a spin for an enhanced Clojure repl

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,7 @@
-{:user {:dependencies [[slamhound "1.5.5"]
-                       [pjstadig/humane-test-output "0.6.0"]]
+{:user {:dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :plugins [[lein-kibit "0.0.8"]
-                  [com.jakemccrary/lein-test-refresh "0.5.5"]]
+                  [com.jakemccrary/lein-test-refresh "0.5.5"]
+                  [venantius/ultra "0.1.9"]]
         :repl-options {:prompt (fn [ns] (str "λ " ns " ─➤ "))}
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
+        :ultra {:color-scheme :solarized_dark}}}
