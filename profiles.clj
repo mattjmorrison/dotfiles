c16ce856523d5a3646e4b1c55f1c5a60b2af530b
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-06-15 20:50:24 -0500

    Dependency: Version bumps for lein plugins in profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,9 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :plugins [[lein-kibit "0.0.8"]
-                  [com.jakemccrary/lein-test-refresh "0.6.0"]
-                  [venantius/ultra "0.2.0"]
-                  [lein-cloverage "1.0.2"]
+        :plugins [[lein-kibit "0.1.2"]
+                  [com.jakemccrary/lein-test-refresh "0.9.0"]
+                  [venantius/ultra "0.3.3"]
+                  [lein-cloverage "1.0.6"]
                   [lein-cljfmt "0.1.10"]
                   [jonase/eastwood "0.2.1"]]
         :repl-options {:prompt (fn [ns] (str "λ " ns " ─➤ "))}
