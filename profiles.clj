8ff0b03fdbedeb4aa08720770b9f477a9373a2a9
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2016-06-27 08:43:08 -0500

    Enhancement: Bumped versions in profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,9 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :plugins [[lein-kibit "0.1.2"]
-                  [com.jakemccrary/lein-test-refresh "0.11.0"]
-                  [venantius/ultra "0.3.4"]
+                  [com.jakemccrary/lein-test-refresh "0.15.0"]
+                  [venantius/ultra "0.4.1"]
                   [lein-cloverage "1.0.6"]
-                  [lein-cljfmt "0.3.0"]
-                  [jonase/eastwood "0.2.1"]]
-        :repl-options {:prompt (fn [ns] (str "λ " ns " ─➤ "))}
-        :ultra {:color-scheme :solarized_dark}}}
+                  [lein-cljfmt "0.5.3"]
+                  [jonase/eastwood "0.2.3"]]
+        :repl-options {:prompt (fn [ns] (str "λ " ns " ─➤ "))}}}
