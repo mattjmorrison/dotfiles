5fd575ce03d26a1980e92c0a57a982aed360a715
Davis <dhcrain@users.noreply.github.com>
2020-02-10 21:28:43 -0500

    Update setup.sh

--- install-script/setup.sh
+++ install-script/setup.sh
@@ -1,7 +1,7 @@
 # helpers
-function echo_ok { echo '\033[1;32m'"$1"'\033[0m'; }
-function echo_warn { echo '\033[1;33m'"$1"'\033[0m'; }
-function echo_error  { echo '\033[1;31mERROR: '"$1"'\033[0m'; }
+function echo_ok { echo -e '\033[1;32m'"$1"'\033[0m'; }
+function echo_warn { echo -e '\033[1;33m'"$1"'\033[0m'; }
+function echo_error  { echo -e '\033[1;31mERROR: '"$1"'\033[0m'; }
 
 #==============
 # Install all the packages
