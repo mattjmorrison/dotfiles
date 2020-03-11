cd247c87de630a14a18fa0b4d15fba9a2216588a
mattjmorrison <mattj.morrison@gmail.com>
2020-03-10 21:59:04 -0500

    Peripherial: add dotfiles-update function
    This will allow running `dotfiles-update` from the command line to
    update the dotfiles (in ~/dotfiles) as well as all custom-configs in
    `~/dotfiles/custom-configs`.

--- default-configs/install.sh
+++ default-configs/install.sh
@@ -7,3 +7,11 @@ function dotfiles-add(){
         source "${dest}/install"
     fi
 }
+
+function dotfiles-update(){
+    git -C ~/dotfiles pull origin master
+    for config in `find ~/dotfiles/custom-configs -maxdepth 2 -mindepth 1 -type d -name .git`
+    do
+        git -C $(dirname $config) pull origin master
+    done
+}
