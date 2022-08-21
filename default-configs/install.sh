b295a5e62422122dc021aed39678f3a800a28b26
Matthew J Morrison <mattjmorrison@mattjmorrison.com>
2022-08-20 21:24:47 -0500

    Swapped out hub for gh

--- default-configs/install.sh
+++ default-configs/install.sh
@@ -2,7 +2,7 @@ function dotfiles-add(){
     repo=$1
     name="${repo##*/}"
     dest="${HOME}/dotfiles/custom-configs/${name}"
-    hub clone ${1} ${dest}
+    gh repo clone ${1} ${dest}
     install-custom-config $dest
 }
 
